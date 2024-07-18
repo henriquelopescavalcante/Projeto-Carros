4f5a93be6a68051491e852707b4cb9f5492014e2
henriquelopes <henriquelopescavalcante@gmail.com>
2024-07-18 17:04:45 -0300

    first commit

---
+++ cars/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import pre_save, post_save, post_delete
+from django.db.models import Sum
+from django.dispatch import receiver
+from cars.models import Car, CarInventory
+
+def car_inventory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value
+    )
+
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    car_inventory_update()
+    
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kwargs):
+    car_inventory_update()
